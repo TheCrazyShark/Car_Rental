010ecc48cad3916c8e4fbcc0760f5af90b4d2494
Shane Smith <sas1041@sru.edu>
2021-10-26 19:51:12 -0400

    Correct Main Menu

--- car_rental/main.cpp
+++ car_rental/main.cpp
@@ -3,22 +3,18 @@
 using namespace std;
 
 void main_menu() {
-	string username, password;
 	int user_input;
 
-	cout << "Welcome to the car rental service! Enter 1 if you already have an account, otherwise enter 2 to create an account.";
-//login: add 3 iterations and then end program
+	cout << "Welcome to the car rental service! Enter 1 if you already have an account, otherwise enter 2 to create an account." << endl;
+	cout << "1 - Login" << endl;
+	cout << "2 - Create Account" << endl;
+
 	do {
 		cin >> user_input;
-		if (user_input == 1) {
-			cout << "Please enter your username";
-			cin >> username;
-			cout << "Please enter your password";
-			cin >> password;
-
+		if (user_input == 1) { // Login
 			login();
 		}
-		else if (user_input == 2) {
+		else if (user_input == 2) { // Create account
 			create_account();
 		}
 		else {
@@ -27,8 +23,14 @@ void main_menu() {
 	} while (user_input != 1 || user_input != 2);
 }
 
-void login(){
-	
+void login() {
+	//login: add 3 iterations and then end program
+	string username, password;
+
+	cout << "Please enter your username";
+	cin >> username;
+	cout << "Please enter your password";
+	cin >> password;
 }
 
 void create_account() {
